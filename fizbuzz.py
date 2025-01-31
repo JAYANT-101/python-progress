c1394d70d79cf9d7e5dad52d6f8ad82933fcd2ee
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-02-01 00:01:12 +0530

    python fizbuz2

--- fizbuzz.py
+++ fizbuzz.py
@@ -1,5 +1,7 @@
 for i in range(0, 100):
-    if(i%2==0):
-        print("fiz")
+    if(i%2==0 and i%3==0):
+        print("fiz buz")
     elif(i%3==0):
         print("buzz")
+    elif(i%2==0):
+        print("fiz")
