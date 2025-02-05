4f6c4320791fa5ba4bd31a02856f3ed672d99850
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-02-06 00:30:22 +0530

    python break and continue

---
+++ break_and_continue.py
@@ -0,0 +1,18 @@
+# for i in range(12):
+#     if (i == 10):
+#         break
+#     print("5 X",i+1,"=",5*(i+1))
+# print("out of the loop")
+
+
+# for i in range(12):
+#     if (i == 5):
+#         print("skip")
+#         continue
+#     print("5 X",i+1,"=",5*(i+1))
+i=0
+while True:
+    print(i)
+    i=i+1
+    if(i%100==0):
+        break
