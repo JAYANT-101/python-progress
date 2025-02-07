71af5f2963c2d5cb558d95dc1fef3023c3efca5a
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-02-08 03:12:13 +0530

    python fun

--- funcation.py
+++ funcation.py
@@ -1,9 +1,18 @@
 def calculateGmean(a, b):
  mean=(a*b)/(a+b)
  print(mean)
+def isGreater(a, b):
+ if(a>b):
+  print("a is greater then b")
+ else:
+  print("b is greater then a ")
+ def isLesser(a, b):#will pass without throwing error to do ti leter
+  pass
 a=9
 b=8
 calculateGmean(a, b)
+isGreater(a, b)
 c=6
 t=5
 calculateGmean(c, a)
+isGreater(c, t)
