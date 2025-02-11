32e30fbdf1a449a0e27d0c9114d6993f2863212e
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-02-11 23:00:08 +0530

    python list method

---
+++ listMethod.py
@@ -0,0 +1,20 @@
+l =[1,23,4,5,6]
+print(l)
+l.append(7)
+print(l)
+l.sort(reverse=True)
+print(l)
+l.reverse()
+print(l)
+print(l.index(1))
+print(l.count(1))
+# m=l in this any chane in m will change m
+m=l.copy()
+m[0]=0
+print(l)
+l.insert(1,33)
+print(l)
+m=[12,34,54]
+k=l+m#make a nwe with both l and m
+l.extend(m)
+print(l)
