24cbf60c5fbb2160f5f47f7d9c967fa8c3ccb3bb
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-02-20 00:14:38 +0530

    python else in loop

---
+++ forLoopWhithElse.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print(i)
+else: print("for loop end")#if the loop is stop using break thr else will not work because else on works after thr iteration of the loop is completed
+i=0;
+while(i<7):
+    print(i)
+    i=i+1
+else:print("loop is complete")
