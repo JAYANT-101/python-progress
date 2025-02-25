d484c91e6a4a5b66708fd867838a0e6edbd0b3ea
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-02-25 11:31:38 +0530

    import

---
+++ importUse.py
@@ -0,0 +1,13 @@
+# import math
+# from math import * to import all from the model
+# import math as s
+# from math import pi,sqrt as s
+from math import sqrt,pi
+import math
+from testjs import hi,a #costome modul when using as key use name.name of v to implement
+print(dir(math))
+print(math.nan,type(math.nan))
+result =sqrt(9)*pi
+print(result)
+hi()
+print(a)
