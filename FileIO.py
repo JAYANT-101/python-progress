06be07eb9ede29ba55fbff5ed61361ad0dbe8143
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-03 00:52:31 +0530

    file manegment

---
+++ FileIO.py
@@ -0,0 +1,11 @@
+#raed mode
+# f=open('testjs.py','r')
+# text=f.read()
+# print(text)
+# f.close()
+#write mode
+# f=open('test.py','w')
+# f.write('#Hello, world!')
+# f.close()
+with open('test.py','a') as f:
+    f.write("bababue")
