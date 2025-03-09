a9d4929dbbc0f93c0301cc3ab2b1ba75bf3ab342
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-09 19:34:06 +0530

    SWG

---
+++ SWG.py
@@ -0,0 +1,11 @@
+import random
+print("Enter the 1. snake\n2.water\n3.gun")
+a=int(input())
+b=random.randint(1,3)
+if a==1 and b==3:
+    print("you lose")
+elif a==2 and b==1:
+    print("you lose")
+elif a==3 and b==2:
+    print("you lose")
+else:print("you win")
