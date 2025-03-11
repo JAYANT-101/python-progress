9fa9b04ab7c8bcea5b9c3f25de8debc8d26e64ad
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-11 21:55:17 +0530

    SWG update

--- SWG.py
+++ SWG.py
@@ -2,7 +2,9 @@ import random
 print("Enter the 1. snake\n2.water\n3.gun")
 a=int(input())
 b=random.randint(1,3)
-if a==1 and b==3:
+if a==b:
+    print("tie")
+elif a==1 and b==3:
     print("you lose")
 elif a==2 and b==1:
     print("you lose")
