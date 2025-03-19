8c9a6a020c1085cd73dd9fa1de0d6e884909e844
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-20 00:32:30 +0530

    class Animal:

--- SingleInheritance.py
+++ SingleInheritance.py
@@ -1,10 +1,29 @@
-class God:
-    def __int__(self,name,num):
-        self.name=name
-        self.num=num
-    def Demo(self):
-        print(f'{self.name} is the only true god emperor in {self.num} years')
-class GODv2(God):
-    def demo2(self):
-        print(f'In {self.num} years the only god emperor is {self.name}')
-a=GODv2("jayant",1000000)
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def make_sound(self):
+        print("Sound made by the animal")
+
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+
+    def make_sound(self):
+        print("Bark!")
+class Cat(Animal):
+    def __(self,name,age):
+        Animal.__init__(self,name,species="Cat")
+        self.age=age
+    def make_sound(self):
+        print("Meaawww")
+
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+c = Cat("Caaat",6)
+c.make_sound()
+a = Animal("Dog", "Dog")
+a.make_sound()
