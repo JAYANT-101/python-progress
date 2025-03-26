b8af733456b35030a0f341a2700e25abc4513a1e
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-26 17:28:14 +0530

    shutil modul

---
+++ ShutilModule.py
@@ -0,0 +1,5 @@
+import shutil
+shutil.copy("StringOperation.py","test.txt")
+# shutil.copytree("Magic","DunderMethods.py") TO coppy a folder
+# shutil.move("Magic/DunderMethods.py","DunderMethods.py")
+# shutil.rmtree("Magic") delete a folder
