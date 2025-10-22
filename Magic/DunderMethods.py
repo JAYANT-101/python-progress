502edb10754e053917cd28bfdbd0fee49e8d5f89
Jayant sinha <jayantsinha2005@gmail.com>
2025-10-22 12:04:37 +0530

    some shieeeee

--- Magic/DunderMethods.py
+++ Magic/DunderMethods.py
@@ -1,15 +1,22 @@
-# class buba:
-#     name="Jayant"
-#     def __len__(self):
-#         i=0
-#         for c in self.name:
-#             i=i+1
-#         return i
-# e=buba()
-# print((e.name))
-# print(len(e))
-from poly import buba
-e=buba()
-print(str(e))
-print(repr(e))
-e()
+class Dun:
+    def __init__(self, *number):
+        self.number = number
+    def __str__(self):
+        print(f"This is class Dum {self.number}")
+    def __len__(self):
+        return len(self.number)
+    def __contains__(self, item):
+        return item in self.number
+    def __add__(self, other):
+        return self.number + other.number
+    def __repr__(self):
+        return f"Dun({self.number})"
+    def jj(self):
+        print(self.number)
+    def __iter__(self):
+        return iter(self.number)
+
+dun = Dun(1,2,3)
+print(len(dun))
+for i in dun:
+    print(i)
