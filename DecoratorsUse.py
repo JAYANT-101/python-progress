dca32af7450808b3b52c459bf00190b53fe614cd
Jayant sinha <jayantsinha2005@gmail.com>
2025-10-23 20:44:07 +0530

    some shieeeee

--- DecoratorsUse.py
+++ DecoratorsUse.py
@@ -14,3 +14,23 @@ def add(a,b):
     print(a+b)
 
 add([34],[2])
+
+#-------------------------------------------------------------------------------------------------------------------
+class Dc:
+    def __init__(self, num: int)->None:
+        self._num = num
+    @property
+    def num(self)-> int:
+        return self._num
+    @num.setter
+    def num(self, num)-> None:
+        self._num = num
+    @num.deleter
+    def num(self)-> None:
+        print("deleted")
+        del self._num
+c=Dc(2)
+print(c.num)
+c.num=3
+print(c.num)
+del c.num
