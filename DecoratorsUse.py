83447a79665e78de226201d156fbe80f65c99342
Jayant sinha <jayantsinha2005@gmail.com>
2025-10-23 18:23:22 +0530

    some shieeeee

--- DecoratorsUse.py
+++ DecoratorsUse.py
@@ -1,22 +1,16 @@
-import logging
-def hello(a):
-    def ab(*args,**kwargs):#*argd conversant in to tuple and **kwargs converts to dictionary
-        print("This is th ab def")
-        a(*args,**kwargs)
-        print("This is ab def end")
-    return ab
-@hello
+import time
+def execution_time(func):
+    def warpper(*args, **kargs):
+        print("this is a costume decorator ")
+        start = time.time()
+        print(start)
+        result = func(*args, **kargs)
+        end = time.time()
+        print(end)
+        return result
+    return warpper
+@execution_time
 def add(a,b):
     print(a+b)
-#hello(add)()
-add(3,5)
-# def log(a):
-#     def np(*args,**kwargs):
-#        logging.info(f"Calling {a.__name__} with args {args}, kwargs {kwargs}")
-#        result=a(*args,**kwargs)
-#        logging.info(f"{a.__name__} returned {result}")
-#        return result
-#     return np
-# def my_function(a,b):
-#     return a+b
-# log(my_function(1,3))
+
+add([34],[2])
