de6c496fa91c7a79d01ab9053fcff698895e5741
Jayant sinha <jayantsinha2005@gmail.com>
2025-11-07 00:27:15 +0530

    some shieeeee

--- Context_Managers.py
+++ Context_Managers.py
@@ -8,7 +8,22 @@ class File:
     def __exit__(self, type, value, traceback):
         print("exit")
         self.file.close()
+        # return True this can be done to make the programme to still work even after an exception is raised it but use
+        # carefully only when the exception is handled
 # this return from the __enter__ will be stored in the file ok
 with File("test.txt", "w") as file:
     print("writing")
-    file.write("hello")
+    file.write ("hello")
+    # raise Exception() simulating an exception
+#--------------------------------------------------------------------------------------------------------------------------------------
+from contextlib import contextmanager
+@contextmanager
+def file(fielname, method):
+    print("enter")
+    file = open(fielname, method)
+    yield file
+    file.close()
+    print("exit")
+with file("test.txt", "w") as f:
+    print("writing")
+    f.write ("hello")
