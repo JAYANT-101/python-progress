f5b13120e9f6bc267b6c0852c4474a8bf3860a82
Jayant sinha <jayantsinha2005@gmail.com>
2025-11-11 23:02:28 +0530

    some shieeeee

--- Swimmer_data.py
+++ Swimmer_data.py
@@ -1,2 +1,16 @@
 import os
 import pandas as pd
+tdata={}
+path =r"C:\Users\jayan\PycharmProjects\python-progress/swimmer_data"
+for root, dirs, files in os.walk(path):
+    for file_name in files:
+        label=file_name[:-4].split('-')
+        file_path = os.path.join(root, file_name)  # Construct the full file path
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                content = f.read()
+                data = content.split(',')
+                tdata[tuple(label)] = data
+        except Exception as e:
+            print(f"Error reading {file_path}: {e}")
+print(tdata)
