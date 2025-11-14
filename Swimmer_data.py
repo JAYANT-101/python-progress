b6de3b8a821e545d58f7154fbdfbe63b1fde8003
Jayant sinha <jayantsinha2005@gmail.com>
2025-11-14 23:16:53 +0530

    some shieeeee

--- Swimmer_data.py
+++ Swimmer_data.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 tdata={}
-path =r"C:\Users\jayan\PycharmProjects\python-progress/swimmer_data"
+path =r"C:\Users\jayan\PycharmProjects\python-progress/swimdata"
 for root, dirs, files in os.walk(path):
     for file_name in files:
         label=file_name[:-4].split('-')
@@ -9,10 +9,9 @@ for root, dirs, files in os.walk(path):
         try:
             with open(file_path, 'r', encoding='utf-8') as f:
                 content = f.read()
-                data = content.split(',')
+                data = content.strip().split(',')
                 cpu=tuple(label)
                 tdata[cpu] = data
         except Exception as e:
             print(f"Error reading {file_path}: {e}")
-kk=pd.DataFrame(tdata)
-print(kk)
+print(tdata)
