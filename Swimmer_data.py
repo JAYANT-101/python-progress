d5aa8cfa584cd1362212bd514217667aff01c522
Jayant sinha <jayantsinha2005@gmail.com>
2025-11-15 23:14:45 +0530

    some shieeeee

--- Swimmer_data.py
+++ Swimmer_data.py
@@ -1,15 +1,17 @@
 import os
 import pandas as pd
 tdata={}
-path =r"C:\Users\jayan\PycharmProjects\python-progress/swimdata"
-for root, dirs, files in os.walk(path):
+PATH =r"C:\Users\jayan\PycharmProjects\python-progress/swimdata"
+for root, dirs, files in os.walk(PATH):
     for file_name in files:
         label=file_name[:-4].split('-')
         file_path = os.path.join(root, file_name)  # Construct the full file path
         try:
             with open(file_path, 'r', encoding='utf-8') as f:
                 content = f.read()
-                data = content.strip().split(',')
+                replacements = content.maketrans(":.","  ")
+                new_content = content.translate(replacements)
+                data = new_content.strip().split(',')
                 cpu=tuple(label)
                 tdata[cpu] = data
         except Exception as e:
