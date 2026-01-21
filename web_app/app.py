ac8be5b904a8da28893761f4432d84f908ed0ee5
Jayant sinha <jayantsinha2005@gmail.com>
2026-01-21 23:58:37 +0530

    web app

--- web_app/app.py
+++ web_app/app.py
@@ -3,27 +3,32 @@ import Swimmer_data
 app = Flask(__name__)
 app.secret_key = "you will never guess"
 
+#fornt page
 @app.get("/")
 def index():
     return render_template('index.html',
                            title="Welcome to the Swimclub system",)
 
+#This populates all function with necessary data
 def populate_session():
     if "data" not in session:
         PATH = r"C:\Users\jayan\PycharmProjects\python-progress\swimdata"
         DIR = r"C:\Users\jayan\PycharmProjects\python-progress\swimdatahtml"
         session["data"] = Swimmer_data.file_opener(PATH, DIR)
 
+#This is just a test it. Shows names of all summers
 @app.get("/swimmer")
 def summer():
     populate_session()
     return str(sorted(session["data"]))
 
+#This function gives summer data
 @app.get("/files/<summers>")
 def get_summer_data(summers):
     populate_session()
     return str(session["data"][summers])
 
+#This creates a dropdown menu of names of the summers
 @app.get("/swimmers")
 def display_swimmers():
     populate_session()
@@ -35,6 +40,7 @@ def display_swimmers():
         data=sorted(session["data"]),
     )
 
+#this gives a selection menu for selecting file based on names
 @app.post("/showfiles")
 def display_swimmers_files():
     populate_session()
