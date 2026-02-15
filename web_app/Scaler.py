714343681b3237400976ab0c9b2ab0c50b2b462f
Jayant sinha <jayantsinha2005@gmail.com>
2026-02-15 23:30:12 +0530

    update

--- web_app/Scaler.py
+++ web_app/Scaler.py
@@ -1,4 +1,5 @@
 def scaled_number(v,f_min,f_max, t_min, t_max):
+
     """conver the list of number to their scaled version in the given range this function takes v the value, f_min the
     minimum value, f_max the maximum value in the list, t_min the minimum number to star the scale and t_max the max scale number"""
 
