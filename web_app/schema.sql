3273213925740000667a038e21efa7b0cf211766
Jayant sinha <jayantsinha2005@gmail.com>
2026-04-22 02:08:43 +0530

    Update

--- web_app/schema.sql
+++ web_app/schema.sql
@@ -1,11 +1,10 @@
 CREATE TABLE swimmers (
-        id integer not null primary key autoincrement,
+        id integer not null primary key auto_increment,
         name varchar(32) not null,
         age integer not null
     );
-CREATE TABLE sqlite_sequence(name,seq);
 CREATE TABLE events (
-            id integer not null primary key autoincrement,
+            id integer not null primary key auto_increment,
             distance varchar(16) not null,
             stroke varchar(16) not null
         );
